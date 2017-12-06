09d60c2e9a7b4f16959cc09b06faa63f2dbb12de
Gabriel Carrizo <gab.carrizo@live.com>
2017-12-06 11:00:55 +0100

    script for unfiltering because lazy

---
+++ sql_unfilter.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.argv[1]
+
+input = input.replace(' ', '/**/')
+input = input.replace(',', '/*,*/')
+if 'select' in input:
+    input = input.replace('select','selSELECTect')
+elif 'SELECT' in input:
+    input = input.replace('SELECT','selSELECTect')
+if 'union' in input:
+    input = input.replace('union','uniUNIONon')
+elif 'UNION' in input:
+    input = input.replace('UNION','uniUNIONon')
+
+print input
